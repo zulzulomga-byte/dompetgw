898d27d7dbea5941eaaa8a6c4e268c55d9fb84fb
zulzulomga-byte <zulzulomga@gmail.com>
2026-07-29 12:06:54 +0900

    Add files via upload

---
+++ android/app/src/main/java/com/saku/wallet/MainActivity.java
@@ -0,0 +1,5 @@
+package com.saku.wallet;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.saku.wallet',
+  appName: 'Saku',
+  webDir: 'www'
+};
+
+export default config;
